717a5e4efa2cb17dda3d82d8261ff217c46e6408
0ga0takash1 <ogawatakafumi2814@gmail.com>
2021-09-06 23:02:19 +0900

    候補を実装

--- classify.py
+++ classify.py
@@ -1,19 +1,19 @@
 import os
 
 paths = [
-    "/output_lists/1_Interface_with_outside/_.txt",
-    "/output_lists/2_Functional_requirements/_.txt",
-    "/output_lists/3_Performance_requirements/_.txt",
-    "/output_lists/4_Logical_database_requirements/_.txt",
-    "/output_lists/5_Design_constraints/_.txt",
-    "/output_lists/6_1_Functional_compatibility/_.txt",
-    "/output_lists/6_2_Performance_efficiency/_.txt",
-    "/output_lists/6_3_Compatibility/_.txt",
-    "/output_lists/6_4_Usability/_.txt",
-    "/output_lists/6_5_Reliability/_.txt",
-    "/output_lists/6_6_Security/_.txt",
-    "/output_lists/6_7_Maintainability/_.txt",
-    "/output_lists/6_8_Portability/_.txt"
+    "/output_lists/1_Interface_with_outside/",
+    "/output_lists/2_Functional_requirements/",
+    "/output_lists/3_Performance_requirements/",
+    "/output_lists/4_Logical_database_requirements/",
+    "/output_lists/5_Design_constraints/",
+    "/output_lists/6_1_Functional_compatibility/",
+    "/output_lists/6_2_Performance_efficiency/",
+    "/output_lists/6_3_Compatibility/",
+    "/output_lists/6_4_Usability/",
+    "/output_lists/6_5_Reliability/",
+    "/output_lists/6_6_Security/",
+    "/output_lists/6_7_Maintainability/",
+    "/output_lists/6_8_Portability/"
 ]
 
 Chapters = [
@@ -32,9 +32,13 @@ Chapters = [
     "移植性"
 ]
 
-def add(text, list_num):
-    path = os.getcwd()
-    path += paths[list_num-1]
+def add(text, list_num, out):
+    path_dir = os.getcwd()
+    path_dir += paths[list_num-1]
+    if out == 1: 
+        path = path_dir+"_.txt"
+    else: 
+        path = path_dir+"__.txt"
     fp = open(path, mode='w')
     fp.write(text)
     fp.close()
@@ -44,8 +48,14 @@ def add(text, list_num):
 def print_result():
     for i in range(len(paths)):
         print(Chapters[i])
-        path = os.getcwd()
-        path += paths[i]
+        path_dir = os.getcwd()
+        path_dir += paths[i]
+        path = path_dir+"_.txt"
+        fp = open(path)
+        print(fp.readline())
+        fp.close()
+        print(Chapters[i], "候補")
+        path = path_dir+"__.txt"
         fp = open(path)
         print(fp.readline())
         fp.close()
