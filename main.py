c2a858086e7b58db3569e455cecfb33a21d9b8e7
0ga0takash1 <ogawatakafumi2814@gmail.com>
2021-09-13 11:28:18 +0900

    文章の根幹のみを抽出する機能実装

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import prog_keyword as pk
 # 画面を出力
 # PDFを入力、PDFのPathを取得
 # PDFからChapter配列を作成
-# パスワードから文章を抽出・分配
+# キーワードから文章を抽出・分配
+import nlp
 # 分配した文章を出力
+import classify as cl
+cl.print_result()
--- nlp.py
+++ nlp.py
@@ -13,18 +13,62 @@ def is_specific_syntax(sent):
         if token.head.text == "以下" \
             and token.dep_ != 'nummod':
                 return 1
-
         # reference syntax
         if token.i:
             return 1
     return 0
 
-def is_candi(sent):
+def is_candidate(sent):
     if is_specific_syntax(sent):
         return 1
     return 0
 
-for txt in pdf_read.text:
-    doc = nlp(txt)
+def subject_is_human(doc):
+    if doc:
+        return 1
+    return 0
+
+def correct_dep_(dep):
+    if dep == 'case' \
+        or dep == 'aux' \
+        or dep == 'obj' \
+        or dep == 'obl' \
+        or dep == 'compound':
+        return 1
+    return 0
+
+def make_root_of_sent(root_text):
+    res = ''   
+    for sent in root_text:
+        if sent[1] == 1:
+            res += sent[0]
+    return res
+
+def root_of_sent(doc):
+    root_text = []
     for sent in doc.sents:
-        classify.classify_to_txt(txt, sent, is_candi(sent))
+        for token in sent:
+            root_text.append([token.text, 0])
+            if token.dep_ == 'ROOT':
+                root_text[token.i][1] = 1
+        for token in sent:
+            if token.head.dep_ == 'ROOT':
+                root_text[token.i][1] = 1
+        add_to_root = 10
+        while add_to_root > 0:
+            add_to_root = 0
+            for token in sent:
+                if root_text[token.head.i][1] == 1 \
+                    and token.dep_ != 'ROOT':
+                    if correct_dep_(token.dep_) == 1:
+                        if root_text[token.i][1] != 1:
+                            root_text[token.i][1] = 1
+                            add_to_root += 1
+    return make_root_of_sent(root_text)
+
+for txt in pdf_read.text:
+    doc_ori = nlp(txt)
+    doc = nlp(root_of_sent(doc_ori))
+    for sent in doc.sents:
+        if subject_is_human(doc) == 0:
+            classify.classify_to_txt(txt, sent, is_candidate(sent))
