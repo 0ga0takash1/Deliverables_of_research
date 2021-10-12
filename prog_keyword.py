a568fd6b4487f77bf8adb1257de633b2ab46044d
0ga0takash1 <ogawatakafumi2814@gmail.com>
2021-10-13 06:35:43 +0900

    分配要素変更

--- prog_keyword.py
+++ prog_keyword.py
@@ -6,7 +6,7 @@ paths = [
     "/keyword_lists/3_Performance_requirements/_.txt",
     "/keyword_lists/4_Logical_database_requirements/_.txt",
     "/keyword_lists/5_Design_constraints/_.txt",
-    "/keyword_lists/6_1_Functional_compatibility/_.txt",
+    "/keyword_lists/6_1_1_Functional_compatibility/_.txt",
     "/keyword_lists/6_2_Performance_efficiency/_.txt",
     "/keyword_lists/6_3_Compatibility/_.txt",
     "/keyword_lists/6_4_Usability/_.txt",
